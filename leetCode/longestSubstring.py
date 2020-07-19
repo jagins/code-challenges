e548eacfdae0c2eeebad2fb3ae76f135a664591c
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-07-19 13:33:48 -0500

    added solution for longest substring from leetcode

---
+++ leetCode/longestSubstring.py
@@ -0,0 +1,12 @@
+def lengthOfLongestSubstring(self, s: str) -> int:
+    used = {} #stores chars being used
+    longest = 0 #length of the longest substring
+    start_index = 0 #starting index of the current substring
+        
+    for i, char in enumerate(s):
+        if char in used:
+            start_index = max(start_index, used[char] + 1)
+            used[char] = i
+            longest = max(longest, i - start_index + 1)
+    return longest
+        
